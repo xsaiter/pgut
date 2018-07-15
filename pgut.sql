30fafc590e405210024cdea6e35f704b3678dcfe
xsaiter <xsaiter@mail.ru>
2018-07-15 09:12:10 +0300

    improve

--- pgut.sql
+++ pgut.sql
@@ -1,4 +1,25 @@
-select '-100.1221e-308' ~ '^-?(0|[1-9]\d*)?\.?\d+(e-?([1-2]\d\d|[3]0[0-8]))?$'
+-----------------------------------------
 
--- domain
-select 'http://www.site.com' ~ '^(http(s)?://)?(www\.)?((\w|-)+\.[a-z]{2,3})$'
+create or replace function validate_domain_name(s text) returns boolean as
+$$
+begin
+return s ~ '^(http(s)?://)?(www\.)?((\w|-)+\.[a-z]{2,3})$';
+end
+$$
+language plpgsql immutable;
+
+-- example: select validate_domain_name('http://www.site.ru')
+
+-----------------------------------------
+
+create or replace function validate_dbl(s text) returns boolean as
+$$
+begin
+return s ~ '^-?(0|[1-9]\d*)?\.?\d+(e-?([1-2]\d\d|[3]0[0-8]))?$';
+end
+$$
+language plpgsql immutable;
+
+-- example: select validate_dbl('-100.1221e-308')
+
+-----------------------------------------
